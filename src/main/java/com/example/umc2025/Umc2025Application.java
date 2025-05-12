92e5bbd27c961d7489f72737203cc74f5a06326c
leesuyong849 <toosee1005@cau.ac.kr>
2025-05-12 16:50:20 +0900

    누락된 코드 추가

--- src/main/java/com/example/umc2025/Umc2025Application.java
+++ src/main/java/com/example/umc2025/Umc2025Application.java
@@ -1,5 +1,6 @@
 package com.example.umc2025;
 
+import com.example.umc2025.service.StoreService.StoreQueryService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -30,4 +31,5 @@ public class Umc2025Application {
             storeService.findStoresByNameAndScore(name, score)
                     .forEach(System.out::println);
         };
+    }
 }
