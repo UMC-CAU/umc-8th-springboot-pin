73aaa853fd457f12b7a23e6907c2b9f24f6e822c
leesuyong849 <toosee1005@cau.ac.kr>
2025-05-28 22:06:43 +0900

    FEAT: week10 기본 설정 파일 준비 완료

---
+++ src/main/java/com/example/umc2025/web/controller/MemberViewController.java
@@ -0,0 +1,32 @@
+package com.example.umc2025.web.controller;
+
+
+import com.example.umc2025.web.dto.MemberRequestDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberViewController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signupPage(Model model) {
+        model.addAttribute("memberJoinDto", new MemberRequestDTO.JoinDto());
+        return "signup";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+}
